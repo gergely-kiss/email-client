6775d49c095414a3f247a9cbe697cb9439982486
Kiss Gergely <kiss-gergely@outlook.com>
2020-05-04 12:54:51 +0100

    first commit

---
+++ src/test/java/uk/gergely/kiss/emailclient/EmailClientApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gergely.kiss.emailclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
